c3963b566cd74617377811b55b4874cf662a04a2
Danilo Leocata <37438774+khalld@users.noreply.github.com>
2022-10-03 12:23:26 +0200

    Redirect if not logged

---
+++ client/src/routes/admin/warehouse/detail/+page.js
@@ -0,0 +1,11 @@
+import userStore from '../../../../lib/store/userStore'
+import {get} from 'svelte/store'
+import env from '../../../../lib/store/env.js';
+import { error, redirect } from '@sveltejs/kit';
+
+export async function load(event) {
+    if(get(userStore).loggedIn != true || get(userStore).role != "admin"){
+        throw redirect(307, '/')
+    }
+
+}
