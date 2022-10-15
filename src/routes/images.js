8d61b64ce0fdf99433d1a247ded2cf7d4a8d7274
Danilo Leocata <37438774+khalld@users.noreply.github.com>
2022-10-15 12:35:12 +0200

    Prepared API upload images

--- src/routes/images.js
+++ src/routes/images.js
@@ -1,42 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const Order = require('../db/models/order.js');
-const { v4: uuidv4 } = require('uuid');
 const env = require('../env.js')
 const multer = require('multer');
 const path = require("path");
 const fs = require("fs");
 
-const DIR = 'uploads';
+var upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            const directory = `uploads/${req.params.prodId}`;
 
-
-var storage = multer.diskStorage({
-    // TODO: PIPPO must be dynamic!!
-    destination: function (req, file, cb) {
-        console.log(req)
-        cb(null, 'uploads/pippo');
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname.replaceAll(" ", "")); //+ '-' + file.originalname);
-    }
-
+            if (!fs.existsSync(directory)) {
+                fs.mkdirSync(directory, { recursive: true })
+            }
+            cb(null, directory);
+        },
+        filename: function (req, file, cb) {
+            cb(null, Date.now() + '-' + file.originalname.toLocaleLowerCase().replaceAll(" ", ""));
+        }
+    })
 });
 
-var upload = multer({ storage: storage });
-
 // TODO: è un form-data! Devi provare a vedere come funge
-router.post('/image-upload', upload.single('image'),(req, res) => {
+router.post('/upload/:prodId', upload.single('image'),(req, res) => {
     const image = req.image;
 
     res.status(201)
     res.send({message: 'File uploaded successfully', type: 'info'})
-
-    // res.send(apiResponse({message: 'File uploaded successfully.', image}));
 });
-
-function apiResponse(results){
-    return JSON.stringify({"status": 200, "error": null, "response": results});
-}
 
 router.post('/delete', function (req, res) {
     try {
@@ -48,35 +39,42 @@ router.post('/delete', function (req, res) {
             throw new Error('Must provide image name')
         }
 
-        fs.unlinkSync(path.join(__dirname, `../../${DIR}/${req.body.prodId}/${req.body.imagename}`))
+        fs.unlinkSync(path.join(__dirname, `../../${env.dir.upload}/${req.body.prodId}/${req.body.imagename}`))
         return res.status(200).send('Successfully! Image has been Deleted');
 
     } catch (err){
         res.status(500)
         res.send({message: err.message, type: 'error'})
     }
-
-
-    // if (!req.params.imagename) {
-    //     console.log("No file received");
-    //     return res.status(500).json('error in delete');    
-    // } else {
-    //     console.log('file received');
-    //     console.log(req.params.imagename);
-    //     try {
-    //         fs.unlinkSync(DIR+'/'+req.params.imagename+'.png');
-    //         return res.status(200).send('Successfully! Image has been Deleted');
-    //     } catch (err) {
-    //         // handle the error
-    //         return res.status(400).send(err);
-    //     }
-        
-    // }
-
 });
 
 router.post("/info", (req, res) => {
-    res.sendFile(path.join(__dirname, `../../${DIR}/${req.body.prodId}/${req.body.name}`));
+    res.sendFile(path.join(__dirname, `../../${env.dir.upload}/${req.body.prodId}/${req.body.name}`));
 });
+
+router.get("/list/:prodId", (req, res) => {
+    try {
+        const mainPath = path.join(__dirname, `../../${env.dir.upload}/${req.params.prodId}`)
+        var images = []
+        //passsing directoryPath and callback function
+        fs.readdir(mainPath, function (err, files) {
+            //handling error
+            if (err) {
+                throw new Error('Unable to scan directory: ' + err);
+            } 
+            //listing all files using forEach
+            files.forEach(function (file) {
+                // Do whatever you want to do with the file
+                images.push(file);
+            });
+
+            res.status(200);
+            res.send(JSON.stringify({images: images, tot: images.length}))
+        });
+    } catch (e){
+        res.status(500)
+        res.send({message: e.message, type: 'error'})
+    }
+})
 
 module.exports = router;
